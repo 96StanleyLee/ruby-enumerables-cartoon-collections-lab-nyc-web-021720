1320899d413ae8ebd375a2dc9ead8684050e6ef4
Stanley Lee <sachih@live.com>
2019-11-17 23:33:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,16 @@ def summon_captain_planet(planeteer_calls)
 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+i = 0
+while i<calls.length
+  if calls[i].length > 4
+    return true
 end
+return false
+end
+end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
